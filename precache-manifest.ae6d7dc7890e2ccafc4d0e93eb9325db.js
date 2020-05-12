8e6a092458d9b552515391cb93fed62d63051388
郑天锴 <maoxn@zhengtiankaideMacBook-Pro.local>
2020-05-12 01:16:21 -0700

    Updates

---
+++ precache-manifest.ae6d7dc7890e2ccafc4d0e93eb9325db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d2e7dd82b1f1a19082a3aa79eceff3",
+    "url": "/realDemo/index.html"
+  },
+  {
+    "revision": "00eb6315cb5995bf3984",
+    "url": "/realDemo/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2e7d62888bd6cf277e35",
+    "url": "/realDemo/static/css/main.8686e1f3.chunk.css"
+  },
+  {
+    "revision": "00eb6315cb5995bf3984",
+    "url": "/realDemo/static/js/2.e04140fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/realDemo/static/js/2.e04140fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7d62888bd6cf277e35",
+    "url": "/realDemo/static/js/main.9b392378.chunk.js"
+  },
+  {
+    "revision": "7064e5a8eb4bbc5a8e40",
+    "url": "/realDemo/static/js/runtime-main.25c93290.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/realDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
